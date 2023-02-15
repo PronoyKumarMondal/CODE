5cac11b4d367eeb92f15a95f0d6ee771b6fea2c5
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-02-16 02:03:38 +0600

    723 solved

---
+++ 723A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b && a>c)
+    {
+        if(b>c)
+        {
+            cout<<(a-c)<<endl;
+        }
+
+        else{
+            cout<<(a-b)<<endl;
+
+        }
+
+    }
+    else if(b>a && b>c)
+    { 
+        if(a>c)
+        {
+            cout<<(b-c)<<endl;
+        }
+        else{
+            cout<<(b-a)<<endl;
+
+        }
+
+    }
+    else if(c>a && c>b){
+          if(a>b)
+          {
+            cout<<(c-b)<<endl;
+
+          }
+          else{
+            cout<<(c-a)<<endl;
+            
+          }
+
+    }
+
+
+
+    return 0;
+}
