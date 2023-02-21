26c4e7c868358c70c84b702a0ac02cb357a682da
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-02-22 00:59:49 +0600

    codeforces contest 827C

--- 827.cpp
+++ 827.cpp
@@ -6,16 +6,16 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, r = 0;
+        int n, c = 0;
         
         string p;
         for (int i = 0; i < 8; i++)
         {
             cin >> p;
             if (p == "RRRRRRRR")
-                {r = 1;}
+                {c = 1;}
         }
-        if (r == 1)
+        if (c == 1)
         {
             cout << "R" << endl;
         }
