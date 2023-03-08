df12f66eb7e91a2c2aa856d234b1f54205418981
PronoyKumar Mondal <pronoy15-14744@diu.edu.bd>
2023-03-08 17:42:48 +0600

    Cf 1399A a/c

--- 1399A.cpp
+++ 1399A.cpp
@@ -6,7 +6,7 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, f = 0;
+        int n, f = 1;
         cin >> n;
         int a[n];
         for (int i = 0; i < n; i++)
@@ -19,19 +19,22 @@ int main()
         }
         else
         {
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    if(a[i]>a[j]){
+                        int temp=a[i];
+                        a[i]=a[j];
+                        a[j]=temp;
+                    }
+                }
+            }
             for (int i = 0; i < n - 1; i++)
             {
-                if ((a[i + 1] - a[i]) == 1 || a[i + 1] == a[i])
+                if ((a[i + 1] - a[i])>1 )
                 {
-                    f = 1;
-                }
-                else{
-                    f=0;
-                }
-                if(f==0){
+                    f = 0;
                     break;
                 }
-
             }
             if (f == 1)
             {
